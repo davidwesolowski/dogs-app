6f29686a6594dfbd932cb98cb3f8b07a8ed7f802
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2023-03-15 11:41:07 +0100

    feature: add list animation

--- src/components/breeds-list/breeds-list.component.tsx
+++ src/components/breeds-list/breeds-list.component.tsx
@@ -1,9 +1,9 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import { useBreeds } from "../../core/breeds/use-breeds.hook"
 import { BreedItem } from "../breed-item/breed-item.component";
 import { Error } from "../error/error.component";
 import { Loader } from "../loader/loader.component";
-import { Modal } from "../modal/modal.component";
 import { SelectedBreedModal } from "../selected-breed-modal/selected-breed-modal.component";
 import classes from './breeds-list.module.scss'
 
@@ -22,11 +22,19 @@ export const BreedList = (): JSX.Element => {
     return (
         <>
             <div className={classes.breedsList}>
-                {
-                    breeds.map((breed) => (
-                        <BreedItem key={breed} breedName={breed} setSelectedBreed={setSelectedBreed} />
-                    ))
-                }
+                <AnimatePresence>
+                    {
+                        breeds.map((breed, idx) => (
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1, transition: { delay: 0.08 * idx } }}
+                                key={breed}>
+                                <BreedItem breedName={breed} setSelectedBreed={setSelectedBreed} />
+                            </motion.div>
+                            
+                        ))
+                    }
+                </AnimatePresence>
             </div>
             <SelectedBreedModal selectedBreed={selectedBreed} handleClose={() => setSelectedBreed(null)} />
         </>
